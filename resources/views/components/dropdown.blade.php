a2970a38dfe30056ada37dff4b8852ac5be1953f
Ian Travers <mr.itpe@gmail.com>
2021-04-29 09:57:31 +0300

    specify right for dropdown component

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -3,7 +3,7 @@
 @php
     $alignmentClasses = [
         'left' => '',
-        'right' => 'right-10',
+        'right' => 'right-6 md:right-10 xl:right-16',
     ]
 @endphp
 
@@ -24,7 +24,7 @@
         x-transition:leave="transition ease-in duration-150 transform"
         x-transition:leave-start="opacity-100 scale-100"
         x-transition:leave-end="opacity-0 scale-95"
-        class="{{ $alignmentClasses[$alignment] }} absolute mt-2 w-48 rounded-md border border-gray-700 py-1 bg-nfsu-color ring-1 ring-white ring-opacity-20 right-"
+        class="{{ $alignmentClasses[$alignment] }} absolute mt-2 w-48 rounded-md border border-gray-700 py-1 bg-nfsu-color ring-1 ring-white ring-opacity-20 md:ri"
         role="menu"
         aria-orientation="vertical"
         aria-labelledby="user-menu"
