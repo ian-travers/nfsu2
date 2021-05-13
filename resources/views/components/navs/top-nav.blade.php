71100dc1ec0942298321e5a694ebb3261ac9cfd1
Ian Travers <mr.itpe@gmail.com>
2021-05-13 15:55:37 +0300

    Top navigation user's avatar && some translations

--- resources/views/components/navs/top-nav.blade.php
+++ resources/views/components/navs/top-nav.blade.php
@@ -30,9 +30,25 @@
             <x-dropdown>
                 <x-slot name="trigger">
                     <button
-                        class="hover:bg-gray-700 hover:text-blue-100 px-3 py-2 rounded-md font-medium {{ substr(Route::currentRouteName(), 0, 6) == 'user' ? 'bg-gray-900 text-white' : 'text-blue-200' }}"
+                        class="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                        id="user-menu"
+                        aria-haspopup="true"
                     >
-                        {{ auth()->user()->username }}
+                        <span class="sr-only">Open user menu</span>
+                        @if(auth()->user()->hasAvatar())
+                            <img
+                                class="h-10 w-10 rounded-full"
+                                src="{{ Storage::url(auth()->user()->avatar) }}"
+                                alt="avatar"
+                            >
+                        @else
+                            <span class="inline-block h-10 w-10 rounded-full overflow-hidden bg-gray-100">
+                                <svg class="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
+                                    <path
+                                        d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z"/>
+                                </svg>
+                            </span>
+                        @endif
                     </button>
                 </x-slot>
                 <x-dropdown-link href="{{ route('settings.profile') }}">{{ __('Settings') }}</x-dropdown-link>
