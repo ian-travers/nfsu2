0ed064c37a0e9d76c1ef5cf5bd7b54946f1c88f8
Ian Travers <mr.itpe@gmail.com>
2021-06-16 15:21:21 +0300

    Public profile link

--- resources/views/components/user-settings/header.blade.php
+++ resources/views/components/user-settings/header.blade.php
@@ -3,6 +3,8 @@
         <p class="text-2xl sm:text-3xl lg:text-4xl text-white font-semibold">{{ $username }}</p>
         <p class="text-xs sm:text-sm lg:text-md text-gray-400">{{ __('Your personal account') }}</p>
     </div>
-    <x-form.primary-button class="mr-4">{{ __('Go to your public profile') }}</x-form.primary-button>
+    <a href="{{ route('public-profile', auth()->user()) }}">
+        <x-form.primary-button class="mr-4" href="">{{ __('Go to your public profile') }}</x-form.primary-button>
+    </a>
 </div>
 
