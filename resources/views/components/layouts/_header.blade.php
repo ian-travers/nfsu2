f985238c1adee0692c44ce36ecbc7ee2abc571c8
Ian Travers <mr.itpe@gmail.com>
2021-09-09 14:55:51 +0300

    Extract header and footer (layout)

---
+++ resources/views/components/layouts/_header.blade.php
@@ -0,0 +1,6 @@
+<header class="bg-nfsu-color border-b border-gray-600">
+    <nav class="max-w-screen-2xl mx-auto">
+        <x-navs.top-nav></x-navs.top-nav>
+        <x-navs.mobile-top-nav></x-navs.mobile-top-nav>
+    </nav>
+</header>
--- resources/views/components/layouts/front.blade.php
+++ resources/views/components/layouts/front.blade.php
@@ -7,17 +7,15 @@
 @livewireStyles
 <body class="antialiased min-h-screen flex flex-col">
 
-<header class="bg-nfsu-color border-b border-gray-600">
-    <nav class="max-w-screen-2xl mx-auto">
-        <x-navs.top-nav></x-navs.top-nav>
-        <x-navs.mobile-top-nav></x-navs.mobile-top-nav>
-    </nav>
-</header>
+<x-layouts._header/>
+
 <main class="flex-grow bg-nfsu-map bg-no-repeat bg-cover bg-fixed text-blue-300">
     @livewire('generic-alert')
 
     {{ $slot }}
 </main>
+
+<x-layouts._footer/>
 
 <script>
     window._locale = '{{ app()->getLocale() }}';
