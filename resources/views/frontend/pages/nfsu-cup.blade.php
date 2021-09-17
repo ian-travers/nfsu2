b8c55a46bb6f203fec00efe32b650c7b4dd08003
Ian Travers <mr.itpe@gmail.com>
2021-09-17 11:19:32 +0300

    About NFSU Cup page (wip)

--- resources/views/frontend/pages/nfsu-cup.blade.php
+++ resources/views/frontend/pages/nfsu-cup.blade.php
@@ -1,5 +1,33 @@
 <x-layouts.front title="{{ $title }}">
     <div class="mt-3 md:mt-4 mx-auto px-4 md:px-8 text-blue-400 max-w-screen-2xl">
         <h2 class="text-xl md:text-3xl my-4 md:my-8 text-center tracking-wider font-medium">{{ __('About NFSU Cup') }}</h2>
+        <div class="space-y-6">
+            <p class="text-base">
+                {{ __('Many years have passed since the release of the game Need for Speed Underground.') }}
+                {{ __('It happened at the end of 2003.') }}
+                {{ __('All the advantages of this game will not be listed here.') }}
+                {{ __('It seems that you already know them.') }}
+                {{ __('In any case, this information is easy to find on the net.') }}
+            </p>
+            <p class="text-base">
+                {{ __('The atmosphere of the game and the soundtrack and customization of the vehicles made a strong impression.') }}
+                {{ __('This, as they say, did not go unnoticed.') }}
+                {{ __('The game has a huge number of fans and many thousands of them went online.') }}
+                {{ __('So it was in 2004. It was impossible to enter the room. There were no places.') }}
+                {{ __('The world does not stand still. Like everything that was popular, it loses its popularity over time.') }}
+                {{ __('Omitting details in 2007, EA show down services for online playing.') }}
+            </p>
+            <p class="text-base">
+                {{ __('NFSU Cup is a project designed for online races on Need for Speed Underground.') }}
+                {{ __('Our mission is to unite all fans of this game.') }}
+            </p>
+            <p class="text-base">
+                {{ __('Join to NFSU Cup.') }}
+            </p>
+        </div>
+
+        <div class="text-center text-xl">
+            4 плашки: Участников ХХ - Гонщиков ХХ - Стран ХХ - Команд ХХ
+        </div>
     </div>
 </x-layouts.front>
