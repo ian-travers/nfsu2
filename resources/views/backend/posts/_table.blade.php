e4f79d8664962aed4bded62809a63228bcc08fab
Ian Travers <mr.itpe@gmail.com>
2021-10-29 09:57:25 +0300

    Publish/Unpublish post (backend)

--- resources/views/backend/posts/_table.blade.php
+++ resources/views/backend/posts/_table.blade.php
@@ -11,6 +11,7 @@
                         <x-table.th>{{ __('Excerpt') }}</x-table.th>
                         <x-table.th class="text-center w-10">{{ __('Published') }}</x-table.th>
                         <x-table.th class="text-center w-10">{{ __('Author') }}</x-table.th>
+                        <x-table.th class="text-center w-10">{{ __('Publishing') }}</x-table.th>
                         <th scope="col" class="w-12 px-6 py-3">
                             <span class="sr-only">Actions</span>
                         </th>
@@ -20,16 +21,41 @@
                     @foreach($posts as $post)
                         <tr class="{{ $post->trashed() ? 'bg-gray-100' : '' }}">
                             <td class="px-6 py-1">
-                                {{ $post->title }}
+                                @if($post->hasImage())
+                                    <div class="flex items-center justify-between">
+                                        {{ $post->title }}
+                                        <img src="{{ Storage::url($post->image) }}" alt="Post image" class="w-32">
+                                    </div>
+                                @else
+                                    {{ $post->title }}
+                                @endif
                             </td>
                             <td class="px-6 py-1">
                                 {{ Str::words($post->excerpt, 8) }}
                             </td>
                             <td class="text-center px-6 py-4">
-                                {{ $post->published_at ? $post->published_at->isoFormat('LL') : __('Draft') }}
+                                {{ $post->published ? $post->published_at->isoFormat('L') : __('Draft') }}
                             </td>
                             <td>
                                 {{ $post->author->username }}
+                            </td>
+                            <td class="text-center">
+                                <form
+                                    action="{{ $post->published ? route('adm.posts.unpublish', $post) : route('adm.posts.publish', $post) }}"
+                                    method="post"
+                                >
+                                    @csrf
+                                    @method('patch')
+
+                                    <button
+                                        type="submit"
+                                        onclick="return confirm()"
+                                        class="items-center px-4 py-2 rounded-md font-semibold text-sm text-white tracking-widest transition {{ $post->published ? 'bg-yellow-600 hover:bg-yellow-700 focus:bg-yellow-700' : 'bg-blue-500 hover:bg-blue-700 focus:bg-blue-700' }}"
+                                        class=""
+                                    >
+                                        {{ $post->published ? __('Unpublish') : __('Publish') }}
+                                    </button>
+                                </form>
                             </td>
                             <td class="whitespace-nowrap text-right text-sm font-medium px-6">
                                 <a
