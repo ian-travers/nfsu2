744742e23b26caa38910ecf1abfe595aa006fca3
Ian Travers <mr.itpe@gmail.com>
2021-11-25 15:44:40 +0300

    Update notifications table (hide read at column less sm width)

--- resources/views/frontend/user/notifications/_table.blade.php
+++ resources/views/frontend/user/notifications/_table.blade.php
@@ -8,7 +8,7 @@
                     <thead>
                     <tr>
                         <x-table.th>{{ __('Notification content') }}</x-table.th>
-                        <x-table.th class="text-center w-40">{{ __('Read at') }}</x-table.th>
+                        <x-table.th class="hidden sm:table-cell text-center w-40">{{ __('Read at') }}</x-table.th>
                         <x-table.th class="text-center w-16">{{ __('Actions') }}</x-table.th>
 
                     </tr>
@@ -44,7 +44,7 @@
                                 @endif
 
                             </td>
-                            <td class="text-center px-6 py-4">
+                            <td class="hidden sm:table-cell text-center px-6 py-4">
                                 {{ $notification->read() ? $notification->read_at->isoFormat('ll') : '' }}
                             </td>
                             <td class="whitespace-nowrap text-right text-sm font-medium px-6">
