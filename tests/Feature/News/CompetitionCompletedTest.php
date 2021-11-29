3d2daa43649b4bf7ea787c3939ded920aad1e948
Ian Travers <mr.itpe@gmail.com>
2021-11-29 14:21:11 +0300

    Fix News\CompetitionCompletedTest

--- tests/Feature/News/CompetitionCompletedTest.php
+++ tests/Feature/News/CompetitionCompletedTest.php
@@ -13,9 +13,10 @@ class CompetitionCompletedTest extends TestCase
     /** @test */
     function when_a_competition_completed_related_news_item_is_created()
     {
-        $this->withoutExceptionHandling();
         /** @var Competition $competition */
         $competition = Competition::factory()->create();
+
+        $this->assertDatabaseCount('news', 1);
 
         $this->signIn(User::factory()->admin()->create());
 
@@ -23,7 +24,7 @@ class CompetitionCompletedTest extends TestCase
             ->set('competition', $competition)
             ->call('handle');
 
-        $this->assertDatabaseCount('news', 1);
+        $this->assertDatabaseCount('news', 2);
         $this->assertDatabaseHas('news', ['title_en' => "Current competition has been completed"]);
     }
 }
