b66a034da83d5a942edc459058d02b95e4facc5e
Ian Travers <mr.itpe@gmail.com>
2021-12-12 17:11:44 +0300

    Dialogue: show message datetime

--- resources/views/components/dialogue/message.blade.php
+++ resources/views/components/dialogue/message.blade.php
@@ -3,7 +3,8 @@
 @if(auth()->user()->is($message->user))
     <div>
         <div class="w-5/6 float-right bg-green-200 border border-green-400 rounded-lg mb-3 px-4 py-2">
-            {{ $message->body }}
+                {{ $message->body }}
+                <span class="block text-xs text-gray-600 text-right">{{ $message->created_at->isoFormat('lll') }}</span>
         </div>
     </div>
 @else
@@ -13,6 +14,7 @@
         </div>
         <div class="bg-blue-200 border border-blue-400 rounded-lg px-4 py-2">
             {{ $message->body }}
+            <span class="block text-xs text-gray-600 text-right">{{ $message->created_at->isoFormat('lll') }}</span>
         </div>
     </div>
 @endif
