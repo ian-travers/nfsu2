91814853799764207f078f72d8dd937317499747
Ian Travers <mr.itpe@gmail.com>
2021-12-24 10:29:44 +0300

    Footer socials links

--- resources/views/components/layouts/_footer.blade.php
+++ resources/views/components/layouts/_footer.blade.php
@@ -11,22 +11,14 @@
                 </div>
 
                 <div class="flex justify-center xl:justify-start space-x-6">
-                    <x-link href="#">
-                        <span class="sr-only">Facebook</span>
-                        <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                            <path fill-rule="evenodd"
-                                  d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
-                                  clip-rule="evenodd"/>
-                        </svg>
-                    </x-link>
-                    <x-link href="#">
+                    <x-link href="https://www.youtube.com/channel/UCyrvLVj8_UizMKjjCA2SZtw">
                         <span class="sr-only">Youtube</span>
                         <svg class="h-6 w-6" fill="currentColor" aria-hidden="true" viewBox="0 0 50 50">
                             <path
                                 d="M 44.898438 14.5 C 44.5 12.300781 42.601563 10.699219 40.398438 10.199219 C 37.101563 9.5 31 9 24.398438 9 C 17.800781 9 11.601563 9.5 8.300781 10.199219 C 6.101563 10.699219 4.199219 12.199219 3.800781 14.5 C 3.398438 17 3 20.5 3 25 C 3 29.5 3.398438 33 3.898438 35.5 C 4.300781 37.699219 6.199219 39.300781 8.398438 39.800781 C 11.898438 40.5 17.898438 41 24.5 41 C 31.101563 41 37.101563 40.5 40.601563 39.800781 C 42.800781 39.300781 44.699219 37.800781 45.101563 35.5 C 45.5 33 46 29.398438 46.101563 25 C 45.898438 20.5 45.398438 17 44.898438 14.5 Z M 19 32 L 19 18 L 31.199219 25 Z"/>
                         </svg>
                     </x-link>
-                    <x-link href="#">
+                    <x-link href="https://twitter.com/NFSUCup">
                         <span class="sr-only">Twitter</span>
                         <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                             <path
